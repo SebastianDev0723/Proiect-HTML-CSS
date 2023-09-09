86d58244c696e2793e170dbd28bfc5401633729e
Stoian Sebastian <stoyan_seby@yahoo.com>
2023-09-09 16:13:01 +0300

    Proeict

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// document.addEventListener("scroll", function() {
+//   const element = document.querySelector(".poza-suplimente");
+//   const rect = element.getBoundingClientRect();
+
+//   if (rect.top < window.innerHeight) {
+//     element.style.animation = "slide"; 
+//   }
+// });
